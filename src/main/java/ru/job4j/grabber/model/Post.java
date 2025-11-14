f3f3f660d243c9260df887262acefb5eaca275ae
dmtrots <dmitrytrotss@gmail.com>
2025-11-14 13:56:41 +0500

    Модель данных - Post. [#285211 #554817]

--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -77,15 +77,12 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(link, post.link)
-                && Objects.equals(title, post.title)
-                && Objects.equals(description, post.description)
-                && Objects.equals(time, post.time);
+        return Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, link, description, time);
+        return Objects.hash(link);
     }
 
     @Override
